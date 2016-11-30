c482a7e6990e8e3ac145433ac6c9199e48471288
wangzheng2013 <wz2013013338@gmail.com>
2016-11-30 22:20:56 +0800

    delete pyc

--- service/service/settings.py
+++ service/service/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
        "ENGINE": "django.db.backends.mysql",
         "NAME": "werewolf",
         "USER": "root",
-        "PASSWORD": "chen134920",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
